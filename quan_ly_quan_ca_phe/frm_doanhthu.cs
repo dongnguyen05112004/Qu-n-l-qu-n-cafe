6b50f8ef6a816c9fa168363ba5127825e34c3420
gnort <tronglehuu54@gmail.com>
2025-07-02 09:00:36 +0700

    Trong first commit

---
+++ quan_ly_quan_ca_phe/frm_doanhthu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quan_ly_quan_ca_phe
+{
+    public partial class frm_doanhthu : Form
+    {
+        public frm_doanhthu()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
